4a0d952d32a654a11b92a6ecb3fbae0afa9b4c77
Victor <hidden@1337.com>
2024-09-08 11:12:25 +0200

    Continue working on tacide.rs

--- tacide.rs
+++ tacide.rs
@@ -1,7 +1,17 @@
+enum BinOps {
+    And,
+    Or,
+    Not,
+    /* We assume other operators
+    (implies, xor, ...) could be
+    constructed with those 3. */
+}
+
 struct Variable;
 
 struct Predicate {
     variables: Vec<Variable>,
+    body: Vec<Box<Self>>,
 }
 
 fn main() {}
